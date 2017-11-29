fc506a599f220eebd8595f2bdeb0acf358621920
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-11-29 23:32:43 +0900

    support tables (#3)
    only `file` table is supported for the moment

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -8,13 +8,73 @@ aliases = "/etc/mail/aliases"
 ports   = [25]
 default_user = "root"
 default_group = "wheel"
+virtual_user = {
+  name: "vmail",
+  group: "vmail",
+  home: "/var/vmail"
+}
 
+tables = [
+  { path: "/etc/mail/domains",
+    name: "domains",
+    type: "file",
+    mode: 644,
+    owner: default_user,
+    group: default_group,
+    matches: [/^example\.org$/, /^example\.net$/] },
+  { path: "/etc/mail/virtuals",
+    name: "virtuals",
+    type: "file",
+    mode: 640,
+    owner: default_user,
+    group: virtual_user[:group],
+    matches: [
+      /^#{Regexp.escape("abuse@example.org john@example.org")}$/,
+      /^#{Regexp.escape("postmaster@example.org john@example.org")}$/,
+      /^#{Regexp.escape("john@example.org #{virtual_user[:name]}")}$/,
+      /^#{Regexp.escape("abuse@example.net john@example.net")}$/,
+      /^#{Regexp.escape("postmaster@example.net john@example.net")}$/,
+      /^#{Regexp.escape("john@example.net #{virtual_user[:name]}")}$/
+    ] }
+]
+
+describe group(virtual_user[:group]) do
+  it { should exist }
+end
+
+describe user(virtual_user[:name]) do
+  it { should exist }
+  it { should belong_to_group virtual_user[:group] }
+  it { should belong_to_primary_group virtual_user[:group] }
+  it { should have_home_directory virtual_user[:home] }
+  it { should have_login_shell "/sbin/nologin" }
+end
 describe file(config_dir) do
   it { should exist }
   it { should be_directory }
   it { should be_mode 755 }
   it { should be_owned_by default_user }
   it { should be_grouped_into default_group }
+end
+
+describe file(virtual_user[:home]) do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by virtual_user[:name] }
+  it { should be_grouped_into virtual_user[:group] }
+  it { should be_mode 755 }
+end
+
+tables.each do |t|
+  describe file(t[:path]) do
+    it { should exist }
+    it { should be_owned_by t[:owner] }
+    it { should be_grouped_into t[:group] }
+    it { should be_mode t[:mode] }
+    t[:matches].each do |m|
+      its(:content) { should match m }
+    end
+  end
 end
 
 describe file(config) do
@@ -24,9 +84,13 @@ describe file(config) do
   it { should be_owned_by default_user }
   it { should be_grouped_into default_group }
   its(:content) { should match(/^table aliases file:#{Regexp.escape(aliases)}$/) }
-  its(:content) { should match(/^listen on lo0$/) }
-  its(:content) { should match(/^accept for local alias <aliases> deliver to mbox$/) }
-  its(:content) { should match(/^accept from local for any relay$/) }
+  tables.each do |t|
+    its(:content) { should match(/^table #{t[:name]} #{t[:type]}:#{t[:path]}$/) }
+  end
+  its(:content) { should match(/^listen on lo0 port 25$/) }
+  # rubocop:disable Style/FormatStringToken
+  its(:content) { should match(/^#{Regexp.escape("accept from any for domain <domains> virtual <virtuals> \\")}\n\s+#{Regexp.escape("deliver to maildir \"#{virtual_user[:home]}/%{dest.domain}/%{dest.user}/Maildir\"")}$/) }
+  # rubocop:enable Style/FormatStringToken
 end
 
 describe command("rcctl get smtpd flags") do
