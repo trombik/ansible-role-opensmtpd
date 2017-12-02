89ffb446fe5d008efff4263f0da9d3d1aae08948
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-12-02 20:57:06 +0900

    [bugfix] support Ubuntu

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -5,7 +5,7 @@ service = "smtpd"
 config_dir = "/etc/mail"
 ports = [25]
 default_user = "root"
-default_group = "wheel"
+default_group = "root"
 user = "_smtpd"
 group = "_smtpd"
 virtual_user = {
@@ -16,8 +16,17 @@ virtual_user = {
 extra_group = ["nobody"]
 
 case os[:family]
+when "ubuntu"
+  config_dir = "/etc"
+  user = "opensmtpd"
+  group = "opensmtpd"
+  extra_group = ["games"]
+  service = "opensmtpd"
 when "freebsd"
   config_dir = "/usr/local/etc/mail"
+  default_group = "wheel"
+when "openbsd"
+  default_group = "wheel"
 end
 
 config = "#{config_dir}/smtpd.conf"
@@ -28,14 +37,14 @@ tables = [
     type: "file",
     mode: 640,
     owner: "root",
-    group: "_smtpd",
+    group: group,
     matches: [/^#{Regexp.escape("john@example.org $2b$08$")}.*$/] },
   { path: "#{config_dir}/aliases",
     name: "aliases",
     type: "file",
     mode: 644,
-    owner: "root",
-    group: "wheel",
+    owner: default_user,
+    group: default_group,
     matches: [
       /^MAILER-DAEMON: postmaster$/,
       /^foo: error:500 no such user$/,
@@ -93,6 +102,15 @@ when "freebsd"
       its(:content) { should match(/^#{s}_enable='NO'$/) }
     end
   end
+end
+
+
+describe file(config_dir) do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 755 }
+  it { should be_owned_by default_user }
+  it { should be_grouped_into default_group }
 end
 
 describe group(group) do
@@ -167,7 +185,13 @@ describe file(config) do
     path = t[:type] == "db" ? "#{t[:path]}.db" : t[:path]
     its(:content) { should match(/^table #{t[:name]} #{t[:type]}:#{path}$/) }
   end
-  its(:content) { should match(/^listen on lo0 port 25$/) }
+  int_lo = case os[:family]
+           when "ubuntu"
+             "lo"
+           else
+             "lo0"
+           end
+  its(:content) { should match(/^listen on #{int_lo} port 25$/) }
   # rubocop:disable Style/FormatStringToken
   its(:content) { should match(/^#{Regexp.escape("accept from any for domain <domains> virtual <virtuals> \\")}\n\s+#{Regexp.escape("deliver to maildir \"#{virtual_user[:home]}/%{dest.domain}/%{dest.user}/Maildir\"")}$/) }
   # rubocop:enable Style/FormatStringToken
