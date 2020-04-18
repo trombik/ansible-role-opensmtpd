7e3b21d91391d99506b2519007c5c2af704138cd
Tomoyuki Sakurai <y@trombik.org>
2020-04-18 21:50:08 +0900

    bugfix: update box versions, fix specs in x509

--- tests/serverspec/x509_spec.rb
+++ tests/serverspec/x509_spec.rb
@@ -23,25 +23,18 @@ describe port(587) do
   it { should be_listening }
 end
 
-stderr_msg = "depth=0 C = TH, ST = Bangkok, O = Internet Widgits Pty Ltd, CN = a.mx.trombik.org
-verify error:num=18:self signed certificate
-verify return:1
-depth=0 C = TH, ST = Bangkok, O = Internet Widgits Pty Ltd, CN = a.mx.trombik.org
-verify error:num=10:certificate has expired
-notAfter=Dec  1 03:57:09 2018 GMT
-verify return:1
-depth=0 C = TH, ST = Bangkok, O = Internet Widgits Pty Ltd, CN = a.mx.trombik.org
-notAfter=Dec  1 03:57:09 2018 GMT
-verify return:1
-DONE
-"
+stdout_match = "subject=C = TH, ST = Bangkok, O = Internet Widgits Pty Ltd, CN = a.mx.trombik.org"
+case Specinfra.backend.run_command("openssl version").stdout
+when /LibreSSL/
+  stdout_match = "subject=/C=TH/ST=Bangkok/O=Internet Widgits Pty Ltd/CN=a.mx.trombik.org"
+end
 
 # XXX sleep before disconnecting. otherwise, the SMTP banner cannot be
 # captured in the stdout
 describe command "(sleep 3; echo helo localhost)| openssl s_client -connect 127.0.0.1:587" do
-  its(:stderr) { should eq stderr_msg }
-  # its(:stdout) { should match(/^#{Regexp.escape("subject=/C=TH/ST=Bangkok/O=Internet Widgits Pty Ltd/CN=a.mx.trombik.org")}$/) }
-  its(:stdout) { should match(/^#{Regexp.escape("subject=C = TH, ST = Bangkok, O = Internet Widgits Pty Ltd, CN = a.mx.trombik.org")}$/) }
+  # XXX here stderr is not tested because the stderr outputs vary depeding on
+  # openssl version.
+  its(:stdout) { should match(/^#{Regexp.escape(stdout_match)}$/) }
   its(:stdout) { should match(/^#{Regexp.escape("220 a.mx.trombik.org ESMTP OpenSMTPD")}$/) }
   its(:exit_status) { should eq 0 }
 end
